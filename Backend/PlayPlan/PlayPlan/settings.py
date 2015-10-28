5dc235e4395e3ef0c53449a0f6df294c358ae83c
Zeacone <andreboot42@gmail.com>
2015-10-28 23:21:45 +0800

    Use HTML template renderer.

--- Backend/PlayPlan/PlayPlan/settings.py
+++ Backend/PlayPlan/PlayPlan/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'PlayPlan.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -69,6 +69,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'PlayPlan.wsgi.application'
 
