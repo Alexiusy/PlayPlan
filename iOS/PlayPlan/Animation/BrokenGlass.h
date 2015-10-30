01c5081643a3aad6c640b544ab4467e3a89204d4
Zeacone <andreboot42@gmail.com>
2015-10-30 23:22:15 +0800

    Add animations.

---
+++ iOS/PlayPlan/Animation/BrokenGlass.h
@@ -0,0 +1,16 @@
+//
+//  BrokenGlass.h
+//  PlayPlan
+//
+//  Created by Zeacone on 15/10/30.
+//  Copyright © 2015年 Zeacone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BrokenGlass : UIView
+
+@property (nonatomic) CALayer *mask;
+@property (nonatomic) CAShapeLayer *glassLayer;
+
+@end
---
+++ iOS/PlayPlan/Animation/BrokenGlass.m
@@ -0,0 +1,21 @@
+//
+//  BrokenGlass.m
+//  PlayPlan
+//
+//  Created by Zeacone on 15/10/30.
+//  Copyright © 2015年 Zeacone. All rights reserved.
+//
+
+#import "BrokenGlass.h"
+
+@implementation BrokenGlass
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
