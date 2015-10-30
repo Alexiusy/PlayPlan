4614a9658b95b9ace9ce3009076023b61c8569ca
Zeacone <andreboot42@gmail.com>
2015-10-30 23:32:39 +0800

    Add method to seperate view.

--- iOS/PlayPlan/Animation/BrokenGlass.m
+++ iOS/PlayPlan/Animation/BrokenGlass.m
@@ -10,6 +10,32 @@
 
 @implementation BrokenGlass
 
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self prepareForBrokenLayer];
+    }
+    return self;
+}
+
+/**
+ *  Seprate view into some layers.
+ */
+- (void)prepareForBrokenLayer {
+    NSMutableArray *layers = [NSMutableArray array];
+    
+    for (NSInteger x = 0; x < 20; x++) {
+        for (NSInteger y = 0; y < 40; y++) {
+            CAShapeLayer *shapeLayer = [CAShapeLayer layer];
+        }
+    }
+    
+    for (CAShapeLayer *layer in layers) {
+        [self.layer addSublayer:layer];
+    }
+}
+
 /*
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
