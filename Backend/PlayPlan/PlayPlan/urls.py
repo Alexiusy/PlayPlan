3f2e51ab2827f6ca2c2f4cb5de6fb4ec64be281d
Zeacone <andreboot42@gmail.com>
2015-11-01 22:25:45 +0800

    Add index page url.

--- Backend/PlayPlan/PlayPlan/urls.py
+++ Backend/PlayPlan/PlayPlan/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 import User
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
     url(r'^', include('User.urls')),
 ]
