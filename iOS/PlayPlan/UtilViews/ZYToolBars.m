c3bb593770c331d0262fd4175e9eb91999d0fec5
Zeacone <andreboot42@gmail.com>
2015-11-05 23:03:53 +0800

    Config toolbars.

--- iOS/PlayPlan/UtilViews/ZYToolBars.m
+++ iOS/PlayPlan/UtilViews/ZYToolBars.m
@@ -10,14 +10,11 @@
 
 @implementation ZYTabBar
 
-
-/*
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect {
     // Drawing code
 }
-*/
 
 @end
 
