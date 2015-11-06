0bbc541c2e592d219b0c66176cd3d9bc884e2c08
Zeacone <andreboot42@gmail.com>
2015-11-06 22:46:33 +0800

    Implement customize navigation bar.

--- iOS/PlayPlan/UtilViews/ZYToolBars.m
+++ iOS/PlayPlan/UtilViews/ZYToolBars.m
@@ -18,6 +18,54 @@
 
 @end
 
+
+@implementation UINavigationBar (ZYNavBar)
+
+static char maskKey;
+
+- (UIView *)mask {
+    return objc_getAssociatedObject(self, &maskKey);
+}
+
+- (void)setMask:(UIView *)mask {
+    objc_setAssociatedObject(self, &maskKey, mask, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (void)setBackgroundColor:(UIColor *)backgroundColor {
+    if (!self.mask) {
+        [self setBackgroundImage:[UIImage new] forBarMetrics:UIBarMetricsDefault];
+        self.mask = [[UIView alloc] initWithFrame:CGRectMake(0, -20, [UIScreen mainScreen].bounds.size.width, CGRectGetHeight(self.bounds) + 20)];
+        self.mask.userInteractionEnabled = NO;
+        self.mask.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
+        [self insertSubview:self.mask atIndex:0];
+    }
+}
+
+- (void)setAlpha:(CGFloat)alpha {
+    [[self valueForKey:@"_leftViews"] enumerateObjectsUsingBlock:^(UIView *view, NSUInteger i, BOOL *stop) {
+        view.alpha = alpha;
+    }];
+    
+    [[self valueForKey:@"_rightViews"] enumerateObjectsUsingBlock:^(UIView *view, NSUInteger i, BOOL *stop) {
+        view.alpha = alpha;
+    }];
+    
+    UIView *titleView = [self valueForKey:@"_titleView"];
+    titleView.alpha = alpha;
+}
+
+- (void)setTranslationY:(CGFloat)translationY {
+    self.transform = CGAffineTransformMakeTranslation(0, translationY);
+}
+
+- (void)reset {
+    [self setBackgroundImage:nil forBarMetrics:UIBarMetricsDefault];
+    [self.mask removeFromSuperview];
+    self.mask = nil;
+}
+
+@end
+
 @implementation ZYNavgationBar
 
 - (void)drawRect:(CGRect)rect {
