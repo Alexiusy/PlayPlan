7f3cdc98d4b874d54bbb33a1b7f7fbc5747e87ad
Zeacone <andreboot42@gmail.com>
2015-11-07 23:34:52 +0800

    Add comment.

--- iOS/PlayPlan/PlayPlan.h
+++ iOS/PlayPlan/PlayPlan.h
@@ -10,6 +10,8 @@
 #define PlayPlan_h
 
 #define SCREEN_SIZE [[UIScreen mainScreen] bounds].size
-#define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
+// 十六进制的每一位占用四位二进制，右移的位数根据二进制来判断
+#define UIColorFromHex(hex) [UIColor colorWithRed:((float)((hex & 0xFF0000) >> 16))/255.0 green:((float)((hex & 0xFF00) >> 8))/255.0 blue:((float)(hex & 0xFF))/255.0 alpha:1.0]
+#define UIColorFromRGB(red, green, blue) [UIColor colorWithRed:(red / 255.0) green:(green / 255.0) blue:(blue / 255.0) alpha:1.0]
 
 #endif /* PlayPlan_h */
