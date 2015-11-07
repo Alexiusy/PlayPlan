00ab75a345ba8124af9234bd6eaf22a15a49b87a
Zeacone <andreboot42@gmail.com>
2015-11-07 23:34:20 +0800

    Delete unuseful code.

--- iOS/PlayPlan/AppDelegate.m
+++ iOS/PlayPlan/AppDelegate.m
@@ -18,7 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     [[UINavigationBar appearance] setTintColor:[UIColor whiteColor]];
-    [[UINavigationBar appearance] setBarTintColor:UIColorFromRGB(0x079889)];
+    [[UINavigationBar appearance] setBarTintColor:UIColorFromHex(0x079889)];
 //    [[UINavigationBar appearance] setBarTintColor:[UIColor colorWithRed:0/255.0 green:175/255.0 blue:240/255.0 alpha:1]];
     [[UINavigationBar appearance] setTitleTextAttributes: [NSDictionary dictionaryWithObjectsAndKeys:
                                                            [UIColor whiteColor], NSForegroundColorAttributeName, [UIFont systemFontOfSize:21], NSFontAttributeName, nil]];
