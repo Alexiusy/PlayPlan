2e0ec3fe4dd2be603fef4d1019378fb16058ef17
Zeacone <andreboot42@gmail.com>
2015-11-08 16:08:27 +0800

    Rename ViewController to MainViewController.

---
+++ iOS/PlayPlan/MainViewController.m
@@ -0,0 +1,92 @@
+//
+//  ViewController.m
+//  PlayPlan
+//
+//  Created by Zeacone on 15/10/25.
+//  Copyright © 2015年 Zeacone. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@property (nonatomic) UITableView *tableview;
+
+@end
+
+@implementation MainViewController
+
+#pragma mark - Life Cycle
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+//    [self.navigationController.navigationBar setMaskBackgroundColor:[UIColor clearColor]];
+    [self loadTableView];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:YES];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    [self.navigationController.navigationBar reset];
+}
+
+#pragma mark - Config View
+
+- (void)loadTableView {
+    self.tableview = ({
+        UITableView *tableview = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_SIZE.width, SCREEN_SIZE.height)];
+        UIView *headerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_SIZE.width, 164)];
+        headerView.backgroundColor = [UIColor purpleColor];
+        tableview.tableHeaderView = headerView;
+        tableview.delegate = self;
+        tableview.dataSource = self;
+        tableview;
+    });
+    [self.view addSubview:self.tableview];
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    CGFloat offsetY = scrollView.contentOffset.y;
+    if (offsetY > 0) {
+        if (offsetY >= 44) {
+            [self setNavigationBarTransformProgress:1];
+        } else {
+            [self setNavigationBarTransformProgress:(offsetY / 44)];
+        }
+        
+    } else {
+        [self setNavigationBarTransformProgress:0];
+        self.navigationController.navigationBar.backIndicatorImage = [UIImage new];
+    }
+}
+
+- (void)setNavigationBarTransformProgress:(CGFloat)progress
+{
+    [self.navigationController.navigationBar setTranslationY:(-44 * progress)];
+    [self.navigationController.navigationBar setMaskAlpha:(1-progress)];
+}
+
+#pragma mark - TableView datasource and delegate
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 10;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 4;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell"];
+    }
+    cell.textLabel.text = [NSString stringWithFormat:@"Section = %@, row = %@", @(indexPath.section), @(indexPath.row)];
+    return cell;
+}
+
+@end
