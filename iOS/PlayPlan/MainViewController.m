67f162a48da3c2c42a200e6e6899e61340a9d3ba
Zeacone <andreboot42@gmail.com>
2015-11-09 22:17:03 +0800

    Add pop views.

--- iOS/PlayPlan/MainViewController.m
+++ iOS/PlayPlan/MainViewController.m
@@ -96,8 +96,9 @@
 - (void)addPopView {
     UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, SCREEN_SIZE.height, SCREEN_SIZE.width, SCREEN_SIZE.height)];
     view.backgroundColor = [UIColor redColor];
-    [self.view addSubview:view];
-    [self.view insertSubview:view aboveSubview:self.tableview];
+//    [self.view addSubview:view];
+    [[[UIApplication sharedApplication] keyWindow] addSubview:view];
+//    [self.view insertSubview:view aboveSubview:self.tableview];
     
     [UIView animateWithDuration:5 animations:^{
         view.transform = CGAffineTransformMakeTranslation(0, SCREEN_SIZE.height / 2);
