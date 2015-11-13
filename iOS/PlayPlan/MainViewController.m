86819bf48100551f75fa7d4c3b50a6b7aaac9b0d
Zeacone <andreboot42@gmail.com>
2015-11-13 23:01:26 +0800

    Add pop views.

--- iOS/PlayPlan/MainViewController.m
+++ iOS/PlayPlan/MainViewController.m
@@ -28,7 +28,7 @@
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:YES];
     UIBarButtonItem *leftButton = [[UIBarButtonItem alloc] initWithTitle:@"Add" style:UIBarButtonItemStylePlain target:self action:nil];
-    
+    UINavigationItem *item = [[UINavigationItem alloc] initWithTitle:@"Main"];
     
     [self.navigationController.navigationItem setLeftBarButtonItem:[[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(swipeDrawer)] animated:YES];
 }
