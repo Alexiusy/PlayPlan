2161c855d0980b5aec391db3fc5082f8cb03dc8f
Zeacone <andreboot42@gmail.com>
2015-11-18 23:08:22 +0800

    Add public overlay.

---
+++ iOS/PlayPlan/PublicViews/Overlay.h
@@ -0,0 +1,13 @@
+//
+//  Overlay.h
+//  PlayPlan
+//
+//  Created by Zeacone on 15/11/18.
+//  Copyright © 2015年 Zeacone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Overlay : UIView
+
+@end
---
+++ iOS/PlayPlan/PublicViews/Overlay.m
@@ -0,0 +1,21 @@
+//
+//  Overlay.m
+//  PlayPlan
+//
+//  Created by Zeacone on 15/11/18.
+//  Copyright © 2015年 Zeacone. All rights reserved.
+//
+
+#import "Overlay.h"
+
+@implementation Overlay
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
