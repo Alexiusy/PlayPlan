6f4b67d7f2240bb700e8cbb92081f1321a43b519
Zeacone <andreboot42@gmail.com>
2015-11-19 23:39:47 +0800

    Add side menu.

--- iOS/PlayPlan/PublicViews/SideMenu.m
+++ iOS/PlayPlan/PublicViews/SideMenu.m
@@ -10,6 +10,15 @@
 
 @implementation SideMenu
 
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.backgroundColor = [UIColor redColor];
+    }
+    return self;
+}
+
 /*
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
