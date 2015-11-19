a9d7bf196b9a8fb33e88f49181fb75d6f528d2a7
Zeacone <andreboot42@gmail.com>
2015-11-19 23:28:34 +0800

    Add side menu.

---
+++ iOS/PlayPlan/PublicViews/SideMenu.m
@@ -0,0 +1,21 @@
+//
+//  SideMenu.m
+//  PlayPlan
+//
+//  Created by Zeacone on 15/11/19.
+//  Copyright © 2015年 Zeacone. All rights reserved.
+//
+
+#import "SideMenu.h"
+
+@implementation SideMenu
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
