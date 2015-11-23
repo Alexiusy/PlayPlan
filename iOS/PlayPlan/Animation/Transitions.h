8ce0865347ca3fe79ee87218a939e7bf0f3f8bd7
Zeacone <andreboot42@gmail.com>
2015-11-23 22:05:43 +0800

    Add transition animation.

---
+++ iOS/PlayPlan/Animation/Transitions.h
@@ -0,0 +1,13 @@
+//
+//  Transitions.h
+//  PlayPlan
+//
+//  Created by Zeacone on 15/11/23.
+//  Copyright © 2015年 Zeacone. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Transitions : NSObject
+
+@end
---
+++ iOS/PlayPlan/Animation/Transitions.m
@@ -0,0 +1,13 @@
+//
+//  Transitions.m
+//  PlayPlan
+//
+//  Created by Zeacone on 15/11/23.
+//  Copyright © 2015年 Zeacone. All rights reserved.
+//
+
+#import "Transitions.h"
+
+@implementation Transitions
+
+@end
