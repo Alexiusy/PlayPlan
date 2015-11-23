bc9f074ec025f915a4260b57a57064d25126d2be
Zeacone <andreboot42@gmail.com>
2015-11-23 23:54:58 +0800

    Add transition animation.

--- iOS/PlayPlan/Animation/Transitions.h
+++ iOS/PlayPlan/Animation/Transitions.h
@@ -7,7 +7,8 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
-@interface Transitions : NSObject
+@interface Transitions : NSObject <UIViewControllerAnimatedTransitioning>
 
 @end
--- iOS/PlayPlan/Animation/Transitions.m
+++ iOS/PlayPlan/Animation/Transitions.m
@@ -10,4 +10,12 @@
 
 @implementation Transitions
 
+- (NSTimeInterval)transitionDuration:(id<UIViewControllerContextTransitioning>)transitionContext {
+    return 0.5f;
+}
+
+- (void)animateTransition:(id<UIViewControllerContextTransitioning>)transitionContext {
+    
+}
+
 @end
