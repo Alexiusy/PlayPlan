8d1ef68c3f485205e31fd50cbf10c509ba983fb0
Zeacone <andreboot42@gmail.com>
2015-11-26 00:56:50 +0800

    Add side menu delegate..

--- iOS/PlayPlan/ContainerViewController.h
+++ iOS/PlayPlan/ContainerViewController.h
@@ -14,7 +14,7 @@
 #import "LocationViewController.h"
 #import "ProfileViewController.h"
 
-@interface ContainerViewController : UIViewController <MainDelegate, LocationDelegate, ProfileDelegate>
+@interface ContainerViewController : UIViewController <MainDelegate, LocationDelegate, ProfileDelegate, SideMenuDelegate>
 
 @property (nonatomic, strong) UIStoryboard           *storyBoard;
 @property (nonatomic, strong) MainViewController     *mainController;
