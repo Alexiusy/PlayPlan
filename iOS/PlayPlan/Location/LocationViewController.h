1cd7f6ce1b197824557d0b62830c5932f38e650d
Zeacone <andreboot42@gmail.com>
2015-11-26 00:09:24 +0800

    Add location view controller.

---
+++ iOS/PlayPlan/Location/LocationViewController.h
@@ -0,0 +1,13 @@
+//
+//  LocationViewController.h
+//  PlayPlan
+//
+//  Created by Zeacone on 15/11/26.
+//  Copyright © 2015年 Zeacone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LocationViewController : UIViewController
+
+@end
