fd5b5f21db93368f3e8231e8dafcf3c9e92fee7a
Zeacone <andreboot42@gmail.com>
2015-12-14 21:54:46 +0800

    Delete useless code.

--- iOS/PlayPlan/Explorer/ExplorerViewController.m
+++ iOS/PlayPlan/Explorer/ExplorerViewController.m
@@ -21,9 +21,6 @@
     // Do any additional setup after loading the view.
 }
 
-
-
-
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
